dc6351c2904f7de2198cd82019ef2361ce7dcccf
Hendrik Heinsar <heinhend@greeny.cs.tlu.ee>
2018-01-29 09:19:43 +0200

    Esimene

---
+++ java.java
@@ -0,0 +1,14 @@
+public class java{
+	public static void main(String[] arg){
+		System.out.println("Tere");
+		int temperatuur=-29;
+		if(temperatuur<-28){
+			System.out.println("Külmapüha");
+		} else {
+			System.out.println("Hommikul kooli");
+			}
+		if(temperatuur>20){
+			System.out.println("Soovitan lühikesi varrukaid");
+		}
+	}
+}
